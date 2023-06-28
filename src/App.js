156ff003cbc56945824961e96881c14ebae26bb4
Joshua <joshuaels2003@gmail.com>
2023-06-28 13:23:39 +0200

    Reworked system

--- src/App.js
+++ src/App.js
@@ -6,39 +6,48 @@ import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
-
-  const [pickaxeClicked, setpickaxeClicked] = useState(false);
-  const [pickaxeClickedCount, setpickaxeClickedCount] = useState(0);
-  const [intervalCounter, setIntervalCounter] = useState(0);
-
-  const [drillClicked, setdrillClicked] = useState(false);
-  const [drillClickedCount, setdrillClickedCount] = useState(0);
-  const [drillIntervalCounter, setdrillIntervalCounter] = useState(0);
-
-  const [grinderClicked, setgrinderClicked] = useState(false);
-  const [grinderClickedCount, setgrinderClickedCount] = useState(0);
-  const [grinderIntervalCounter, setgrinderIntervalCounter] = useState(0);
-
-  const [minerClicked, setminerClicked] = useState(false);
-  const [minerClickedCount, setminerClickedCount] = useState(0);
-  const [minerIntervalCounter, setminerIntervalCounter] = useState(0);
-
-  const [corpClicked, setcorpClicked] = useState(false);
-  const [corpClickedCount, setcorpClickedCount] = useState(0);
-  const [corpIntervalCounter, setcorpIntervalCounter] = useState(0);
-
-  const [pickPrice, setpickPrice] = useState(15);
-  const [handPrice, sethandPrice] = useState(500);
-  const [grindPrice, setgrindPrice] = useState(2000);
-  const [minerPrice, setminerPrice] = useState(5000);
-  const [corpPrice, setcorpPrice] = useState(10000);
-
-  const [pickEff, setpickEff] = useState(0.1);
-  const [drillEff, setdrillEff] = useState(1);
-  const [grinderEff, setgrinderEff] = useState(8);
-  const [minerEff, setminerEff] = useState(47);
-  const [corpEff, setcorpEff] = useState(260);
-
+  const [upgrades, setUpgrades] = useState([
+    {
+      name: "Pickaxes",
+      clicked: false,
+      clickedCount: 0,
+      intervalCounter: 0,
+      price: 15,
+      efficiency: 0.1
+    },
+    {
+      name: "Hand Drills",
+      clicked: false,
+      clickedCount: 0,
+      intervalCounter: 0,
+      price: 500,
+      efficiency: 1
+    },
+    {
+      name: "Grinders",
+      clicked: false,
+      clickedCount: 0,
+      intervalCounter: 0,
+      price: 2000,
+      efficiency: 8
+    },
+    {
+      name: "Miners",
+      clicked: false,
+      clickedCount: 0,
+      intervalCounter: 0,
+      price: 5000,
+      efficiency: 47
+    },
+    {
+      name: "Mining Corporations",
+      clicked: false,
+      clickedCount: 0,
+      intervalCounter: 0,
+      price: 10000,
+      efficiency: 260
+    }
+  ]);
 
   let audio = new Audio(stone);
   audio.volume = 0.2;
@@ -65,128 +74,43 @@ function App() {
     setCount(prevCount => prevCount + 1);
   };
 
-  const handlePickClicked = () => {
-    if (count >= pickPrice) {
-      if (!pickaxeClicked) {
-        setpickaxeClicked(true);
-      }
+  const handleUpgradeClick = (index) => {
+    const upgrade = upgrades[index];
+    if (count >= upgrade.price) {
       playSound();
-      setIntervalCounter(prevCounter => prevCounter + 0.1);
-      setpickaxeClickedCount(prevCounter => prevCounter + 1);
-      setCount(prevCount => prevCount - pickPrice);
-      setpickPrice(prevPrice => Math.round(prevPrice + prevPrice * 0.15));
+      setUpgrades((prevUpgrades) => {
+        const newUpgrades = [...prevUpgrades];
+        newUpgrades[index] = {
+          ...newUpgrades[index],
+          clicked: true,
+          clickedCount: newUpgrades[index].clickedCount + 1,
+          intervalCounter: newUpgrades[index].intervalCounter + upgrade.efficiency,
+          price: Math.round(upgrade.price + upgrade.price * 0.15),
+        };
+        return newUpgrades;
+      });
+      setCount((prevCount) => prevCount - upgrade.price);
     } else {
-      alert("Not enough Minerals");
+      alert("Not enough minerals");
     }
   };
 
-  const handleDrillClicked = () => {
-    if (count >= handPrice) {
-      if (!drillClicked) {
-        setdrillClicked(true);
-      }
-      playSound();
-      setdrillIntervalCounter(prevCounter => prevCounter + 1);
-      setdrillClickedCount(prevCounter => prevCounter + 1);
-      setCount(prevCount => prevCount - handPrice);
-      sethandPrice(prevPrice => Math.round(prevPrice + prevPrice * 0.15));
-    } else {
-      alert("Not enough Minerals");
-    }
-  };
-
-  const handleGrinderClicked = () => {
-    if(count >= grindPrice){
-    if (!grinderClicked) {
-      setgrinderClicked(true);
-    }
-    setgrinderIntervalCounter(prevCounter => prevCounter + 8);
-    setgrinderClickedCount(prevCounter => prevCounter + 1);
-    setCount(prevCount => prevCount - grindPrice);
-    setgrindPrice(prevPrice => Math.round(prevPrice + prevPrice * 0.15));
-  }else {
-    alert("Not enough Minerals");
-  }
-  };
-
-  const handleMinerClicked = () => {
-    if(count >= minerPrice){
-    if (!minerClicked) {
-      setminerClicked(true);
-    }
-    setminerIntervalCounter(prevCounter => prevCounter + 47);
-    setminerClickedCount(prevCounter => prevCounter + 1);
-    setCount(prevCount => prevCount - minerPrice);
-    setminerPrice(prevPrice => Math.round(prevPrice + prevPrice * 0.15));
-  }else {
-    alert("Not enough Minerals");
-  }
-  }
-
-  const handleCorpClicked = () => {
-    if(count >= corpPrice) {
-    if (!corpClicked) {
-      setcorpClicked(true);
-    }
-    setcorpIntervalCounter(prevCounter => prevCounter + 47);
-    setcorpClickedCount(prevCounter => prevCounter + 1);
-    setCount(prevCount => prevCount - corpPrice);
-    setcorpPrice(prevPrice => Math.round(prevPrice + prevPrice * 0.15));
-  }else {
-    alert("Not enough Minerals");
-  }
-  }
-
-
   useEffect(() => {
-    let interval;
-    if (pickaxeClicked) {
-      interval = setInterval(() => {
-        setCount(prevCount => prevCount + intervalCounter);
-      }, 1000);
+    if (upgrades) {
+      const intervals = [];
+      upgrades.forEach((upgrade) => {
+        if (upgrade.clicked) {
+          const interval = setInterval(() => {
+            setCount((prevCount) => prevCount + upgrade.intervalCounter);
+          }, 1000);
+          intervals.push(interval);
+        }
+      });
+      return () => {
+        intervals.forEach((interval) => clearInterval(interval));
+      };
     }
-    return () => clearInterval(interval);
-  }, [pickaxeClicked, intervalCounter]);
-
-  useEffect(() => {
-    let interval;
-    if (drillClicked) {
-      interval = setInterval(() => {
-        setCount(prevCount => prevCount + drillIntervalCounter);
-      }, 1000);
-    }
-    return () => clearInterval(interval);
-  }, [drillClicked, drillIntervalCounter]);
-
-  useEffect(() => {
-    let interval;
-    if (grinderClicked) {
-      interval = setInterval(() => {
-        setCount(prevCount => prevCount + grinderIntervalCounter);
-      }, 1000);
-    }
-    return () => clearInterval(interval);
-  }, [grinderClicked, grinderIntervalCounter]);
-
-  useEffect(() => {
-    let interval;
-    if (minerClicked) {
-      interval = setInterval(() => {
-        setCount(prevCount => prevCount + minerIntervalCounter);
-      }, 1000);
-    }
-    return () => clearInterval(interval);
-  }, [minerClicked, minerIntervalCounter]);
-
-  useEffect(() => {
-    let interval;
-    if (corpClicked) {
-      interval = setInterval(() => {
-        setCount(prevCount => prevCount + corpIntervalCounter);
-      }, 1000);
-    }
-    return () => clearInterval(interval);
-  }, [corpClicked, corpIntervalCounter]);
+  }, [upgrades]);
 
 
   return (
@@ -196,42 +120,28 @@ function App() {
         <br />
         {count.toFixed(1)} minerals
       </div>
+      <div className='hr_bar'>
 
+      </div>
 
       <img className="main_button" onClick={handleClick} src={rock}></img>
 
+      <div className='tool_menu' style={{ backgroundImage: 'url("./wood.png")' }}>
+        {upgrades.map((upgrade, index) => (
+          <button key={index} onClick={() => handleUpgradeClick(index)}>
+            {upgrade.clickedCount} {upgrade.name}
+            <br />
+            the price is: {upgrade.price}
+          </button>
+        ))}
+      </div>
       <div className='upgrade_menu' style={{ backgroundImage: 'url("./wood.png")' }}>
-        <button onClick={handlePickClicked}>
-          {pickaxeClickedCount} pickaxes
-          <br />
-          the price is: {pickPrice}
-        </button>
-        <button onClick={handleDrillClicked}>
-          {drillClickedCount} Hand Drills
-          <br />
-          the price is: {handPrice}
-        </button>
-        <button onClick={handleGrinderClicked}>
-          {grinderClickedCount} Grinders
-          <br />
-          the price is: {grindPrice}
-        </button>
-        <button onClick={handleMinerClicked}>
-          {minerClickedCount} Miners
-          <br />
-          the price is: {minerPrice}
-        </button>
-        <button onClick={handleCorpClicked}>
-          {corpClickedCount} Mining Corporations
-          <br />
-          the price is: {corpPrice}
-        </button>
+
       </div>
     </div>
   );
 }
 
 export default App;
-
 
 
